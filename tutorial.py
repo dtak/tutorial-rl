8e3ee89d08792f1a00c81ae009ebbbf4de8cbb2b
finaledoshivelez <finale@seas.harvard.edu>
2016-07-28 22:04:14 -0400

    fixed plotting function

--- tutorial.py
+++ tutorial.py
@@ -40,7 +40,7 @@ simple_maze = [
     '#o......#',
     '#########']
 
-maze = simple_maze 
+maze = long_maze 
 
 task = gridworld.GridWorld( maze ,
                             action_error_prob=.1, 
@@ -72,6 +72,7 @@ def update_SARSA( Q_table , alpha , gamma , state , action , reward , new_state 
 # - changing the epsilon in the epsilon greedy 
 # - on vs. off policy 
 # action error prob vs epsilon 
+# should the episode end when goal is reached? 
 
 # Parameters 
 alpha = .5
@@ -128,57 +129,51 @@ def plot_arrow( location , direction , plot ):
     arrow = plt.arrow( location[0] , location[1] , dx , dy , fc="k", ec="k", head_width=0.05, head_length=0.1 )
     plot.add_patch(arrow) 
 
-# Util to plot the value and the policy     
-def plot_value_and_policy( Q_table , maze ):
-    row_count = len( maze )
-    col_count = len( maze[0] ) 
-    value_function = np.reshape( np.max( Q_table , 1 ) , ( row_count , col_count ) )
-    policy_function = np.reshape( np.argmax( Q_table , 1 ) , ( row_count , col_count ) )
+# Useful stats for the plot
+row_count = len( maze )
+col_count = len( maze[0] ) 
+value_function = np.reshape( np.max( Q_table , 1 ) , ( row_count , col_count ) )
+policy_function = np.reshape( np.argmax( Q_table , 1 ) , ( row_count , col_count ) )
+wall_info = .5 + np.zeros( ( row_count , col_count ) )
+wall_mask = np.zeros( ( row_count , col_count ) )
+for row in range( row_count ):
+    for col in range( col_count ):
+        if maze[row][col] == '#':
+            wall_mask[row,col] = 1     
+wall_info = np.ma.masked_where( wall_mask==0 , wall_info )
 
-    # wall info 
-    wall_info = .5 + np.zeros( ( row_count , col_count ) )
-    wall_mask = np.zeros( ( row_count , col_count ) )
-    for row in range( row_count ):
-        for col in range( col_count ):
-            if maze[row][col] == '#':
-                wall_mask[row,col] = 1     
-    wall_info = np.ma.masked_where( wall_mask==0 , wall_info )
-
-    # value function plot 
-    plt.figure() 
-    plt.imshow( value_function , interpolation='none' , cmap=matplotlib.cm.jet )
-    plt.colorbar()
-    plt.imshow( wall_info , interpolation='none' , cmap=matplotlib.cm.gray )
-    plt.title( 'Value Function' )
-
-    # policy plot 
-    plt.figure()
-    plt.imshow( 1 - wall_mask , interpolation='none' , cmap=matplotlib.cm.gray )    
-    for row in range( row_count ):
-        for col in range( col_count ):
-            if wall_mask[row][col] == 1:
-                continue 
-            if policy_function[row,col] == 0:
-                dx = 0; dy = -.5
-            if policy_function[row,col] == 1:
-                dx = 0; dy = .5
-            if policy_function[row,col] == 2:
-                dx = .5; dy = 0
-            if policy_function[row,col] == 3:
-                dx = -.5; dy = 0
-            plt.arrow( col , row , dx , dy , shape='full', lw=3, length_includes_head=True, head_width=.2 )
-    plt.title( 'Policy' )        
-    plt.show( block=False ) 
-
-# Plot the rewards     
+# Plot the rewards
+plt.subplot( 1 , 3 , 1 ) 
 plt.plot( episode_reward_set.T )
 plt.title( 'Rewards per Episode (each line is a rep)' ) 
 plt.xlabel( 'Episode Number' )
 plt.ylabel( 'Sum of Rewards in Episode' )
-plt.show( block=False )
 
-# Plot the Value functions 
-plot_value_and_policy( Q_table , maze )
+# value function plot 
+plt.subplot( 1 , 3 , 2 ) 
+plt.imshow( value_function , interpolation='none' , cmap=matplotlib.cm.jet )
+plt.colorbar()
+plt.imshow( wall_info , interpolation='none' , cmap=matplotlib.cm.gray )
+plt.title( 'Value Function' )
+
+# policy plot 
+plt.subplot( 1 , 3 , 3 ) 
+plt.imshow( 1 - wall_mask , interpolation='none' , cmap=matplotlib.cm.gray )    
+for row in range( row_count ):
+    for col in range( col_count ):
+        if wall_mask[row][col] == 1:
+            continue 
+        if policy_function[row,col] == 0:
+            dx = 0; dy = -.5
+        if policy_function[row,col] == 1:
+            dx = 0; dy = .5
+        if policy_function[row,col] == 2:
+            dx = .5; dy = 0
+        if policy_function[row,col] == 3:
+            dx = -.5; dy = 0
+        plt.arrow( col , row , dx , dy , shape='full', lw=3, length_includes_head=True, head_width=.2 )
+plt.title( 'Policy' )        
+plt.show( block=False ) 
 
 # If you want to interact with it further... 
 import pdb 
